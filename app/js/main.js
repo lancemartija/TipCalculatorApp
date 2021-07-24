363bbb18ac77a92dfc0e1725a9ba91e7df8d329f
Lance Martija <jacobmartija03@gmail.com>
2021-07-25 00:02:48 +0800

    improve input validation for number of people and bill amount input

--- app/js/main.js
+++ app/js/main.js
@@ -16,17 +16,42 @@ if (button) {
   })
 }
 
-// Input validation for number of people
+// Input validation for number of people and bill amount
 
-const numOfPeople = document.querySelector('#person__input');
-const warningText = document.querySelector('#text-warning');
+const billAmount = document.querySelector('#bill__input');
+const textWarning1 = document.querySelector('#text-warning-1');
+const numOfPeople = document.querySelector('#num-of-ppl__input');
+const textWarning2 = document.querySelector('#text-warning-2');
 
-numOfPeople.onkeyup = function () {
-  if (numOfPeople.value === "0") {
-    warningText.classList.add('warning');
-    numOfPeople.classList.add('warning');
+const isNumeric = (value) => {
+  return !isNaN(parseFloat(value)) && !isNaN(value);
+  // return /^\d+$/.test(value);
+}
+
+const inputValidation = (element, textElement) => {
+  if (element.value === "") {
+    textElement.classList.remove('warning');
+    element.classList.remove('warning');
+  } else if (isNumeric(element.value)) {
+    if (Number(element.value) === 0) {
+      textElement.innerHTML = 'Can\'t be zero';
+      textElement.classList.add('warning');
+      element.classList.add('warning');
+    } else {
+      textElement.classList.remove('warning');
+      element.classList.remove('warning');
+    }
   } else {
-    warningText.classList.remove('warning');
-    numOfPeople.classList.remove('warning');
+    textElement.innerHTML = 'Should be 1 or higher';
+    textElement.classList.add('warning');
+    element.classList.add('warning');
   }
+}
+
+billAmount.onkeyup = () => {
+  inputValidation(billAmount, textWarning1);
+}
+
+numOfPeople.onkeyup = () => {
+  inputValidation(numOfPeople, textWarning2);
 }
