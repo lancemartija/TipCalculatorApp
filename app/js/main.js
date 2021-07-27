cce16a794587a1da3ff0961f384553922b97f8b3
Lance Martija <jacobmartija03@gmail.com>
2021-07-27 16:21:56 +0800

    improve input validation logic

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,13 @@
 // Input Fields
-const billInputField = document.querySelector('#input__bill');
+const billInputField = document.querySelector('#bill__input');
 const peopleInputField = document.querySelector('#num-of-ppl__input');
 const customInputField = document.querySelector('#custom__input');
 
+
 // Buttons
-const tipButtons = document.querySelectorAll('#tip-btn');
+const tipButtons = document.querySelectorAll('#tip-btn'); // includes custom input field
 const resetButton = document.querySelector('#reset-btn');
+
 
 // Text Labels
 const tipAmountLabel = document.querySelector('#tip-amount');
@@ -13,15 +15,54 @@ const totalAmountLabel = document.querySelector('#total');
 const billWarningLabel = document.querySelector('#text-warning-1');
 const peopleWarningLabel = document.querySelector('#text-warning-2');
 
-// Reset Values Function
-const resetAllValues = (inputField, textLabel) => {
 
+// Default Values
+const defaultTipAmountState = tipAmountLabel.innerHTML;
+const defaultTotalAmountState = totalAmountLabel.innerHTML;
+let tipAmount = 0;
+
+
+// Add Class Functions
+const addWarningClass = (element) => {
+  element.classList.add('warning');
 }
+
+const addDisableBtnClass = (element) => {
+  element.classList.add('disabled');
+}
+
+const addWarningMessage1 = (element) => {
+  element.innerHTML = 'Can\'t be zero';
+}
+
+const addWarningMessage2 = (element) => {
+  element.innerHTML = 'Must be a number';
+}
+
+
+// Remove Class Functions
+const removeWarningClass = (element) => {
+  element.classList.remove('warning');
+}
+
+const removeDisableBtnClass = (element) => {
+  element.classList.remove('disabled');
+}
+
+const removeWarningMessage1 = (element) => {
+  element.innerHTML = null;
+}
+
+const removeWarningMessage2 = (element) => {
+  element.innerHTML = null;
+}
+
 
 // Function that returns TRUE if value is Numeric
 const isNumeric = (value) => {
   return !isNaN(parseFloat(value)) && !isNaN(value);
 }
+
 
 // Round Up Numbers to 2
 // roundUptTo2(billInputField.value);
@@ -30,17 +71,67 @@ const roundUpTo2 = (value) => {
   return Math.round((Number(value) * 100)) / 100;
 }
 
-/*
-- add reset values
-- add functions that add and removes classes
-- loop through buttons and assign tip values to each keys
-*/
+
+// Reset Button Logic
+const resetBtnLogic = () => {
+  if (billInputField.value === '' && peopleInputField.value === '' && customInputField.value === '') {
+    addDisableBtnClass(resetButton);
+  } else {
+    removeDisableBtnClass(resetButton);
+  }
+}
 
 
+// Input Validation Function
+const inputValidation = (inputField, warningLabel, resetBtn) => {
+  if (inputField.value === '') {
+    removeWarningClass(inputField);
+    removeWarningClass(warningLabel);
+    removeWarningMessage1(warningLabel);
+    removeWarningMessage2(warningLabel);
+  } else {
+    if (!isNumeric(inputField.value)) {
+      addWarningClass(inputField);
+      addWarningClass(warningLabel);
+      addWarningMessage2(warningLabel);
+    } else if (Number(inputField.value) === 0) {
+      addWarningClass(inputField);
+      addWarningClass(warningLabel);
+      addWarningMessage1(warningLabel);
+    }
+  }
+}
 
+// Onclick/Onkeyup Functions
+billInputField.onkeyup = () => {
+  resetBtnLogic();
+  inputValidation(billInputField, billWarningLabel, resetButton);
+}
 
+peopleInputField.onkeyup = () => {
+  resetBtnLogic();
+  inputValidation(peopleInputField, peopleWarningLabel, resetButton);
+}
 
+customInputField.onkeyup = () => {
+  resetBtnLogic();
+  inputValidation(customInputField, null, resetButton);
+}
 
+resetButton.onclick = () => {
+  billInputField.value = null;
+  peopleInputField.value = null;
+  customInputField.value = null;
+  tipAmountLabel.innerHTML = defaultTipAmountState;
+  totalAmountLabel.innerHTML = defaultTotalAmountState;
+  tip = 0;
+  removeWarningClass(billInputField); // removes 'warning' CLASS from BILL input field
+  removeWarningClass(billWarningLabel); // removes 'warning' LABEL from BILL input field
+  removeWarningClass(peopleInputField); // removes 'warning' CLASS from NUMBER OF PEOPLE field
+  removeWarningClass(peopleWarningLabel); // removes 'warning' CLASS from NUMBER OF PEOPLE input field
+  removeWarningClass(customInputField); // removes 'warning' CLASS from CUSTOM input field
+  addDisableBtnClass(resetButton); // removes 'disabled' CLASS of RESET button
+}
 
 
 
